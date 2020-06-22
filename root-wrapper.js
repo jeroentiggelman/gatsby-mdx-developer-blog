6f3206a9733765e1c68d0e893bfc9198c06ae670
jeroentiggelman <infos@photographicdesign.be>
2020-06-22 17:33:06 +0200

    create Code.js component

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,41 +1,22 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
-import Highlight, { defaultProps } from "prism-react-renderer";
-import theme from "prism-react-renderer/themes/nightOwl";
+import { Code } from "./src/components/pageElements/Code";
 
 const components = {
   h2: ({ children }) => <h2 style={{ color: "rebeccapurple" }}>{children}</h2>,
   "p.inlineCode": (props) => (
     <code {...props} style={{ backgroundColor: "lightgrey" }}></code>
   ),
-  pre: (props) => {
-    const className = props.children.props.className || "";
-    const matches = className.match(/language-(?<lang>.*)/);
-
-    return (
-      <Highlight
-        {...defaultProps}
-        code={props.children.props.children.trim()}
-        language={
-          matches && matches.groups && matches.groups.lang
-            ? matches.groups.lang
-            : ""
-        }
-        theme={theme}
-      >
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
-    );
+  pre: ({ children: { props } }) => {
+    if (props.mdxType === "code") {
+      return (
+        <Code
+          codeString={props.children.trim()}
+          language={props.className && props.className.replace("language-", "")}
+          {...props}
+        />
+      );
+    }
   },
 };
 
