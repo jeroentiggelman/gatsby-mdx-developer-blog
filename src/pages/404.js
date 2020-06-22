e5e074ff6f33a6c5e0cbb23b1277450e0ad1a92b
jeroentiggelman <infos@photographicdesign.be>
2020-06-22 12:02:59 +0200

    add basic 404

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const NotFound = () => {
+  return (
+    <p>
+      Nope, go <Link to="/">home</Link>
+    </p>
+  );
+};
+
+export default NotFound;
