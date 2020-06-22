4fbe1ed5dabdef6bcc5e8f6330458597107a8508
jeroentiggelman <infos@photographicdesign.be>
2020-06-22 10:14:35 +0200

    added content and mdx, filesystem plugins

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+};
