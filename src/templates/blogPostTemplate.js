27b55b245b6e78c3b6b734b9c6f8da627b9aba70
jeroentiggelman <infos@photographicdesign.be>
2020-06-22 13:59:47 +0200

    created blog post pages and pagination

---
+++ src/templates/blogPostTemplate.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { graphql, Link } from "gatsby";
+import { Layout } from "../components/Layout";
+
+export default ({ data, pageContext }) => {
+  const { frontmatter, body } = data.mdx;
+  const { previous, next } = pageContext;
+
+  return (
+    <Layout>
+      <h1>{frontmatter.title}</h1>
+      <p>{frontmatter.date}</p>
+      <MDXRenderer>{body}</MDXRenderer>
+      {previous === false ? null : (
+        <>
+          {previous && (
+            <Link to={previous.fields.slug}>
+              <p>Previous page: {previous.frontmatter.title}</p>
+            </Link>
+          )}
+        </>
+      )}
+      {next === false ? null : (
+        <>
+          {next && (
+            <Link to={next.fields.slug}>
+              <p>Next page: {next.frontmatter.title}</p>
+            </Link>
+          )}
+        </>
+      )}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query PostBySlug($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      body
+      frontmatter {
+        title
+        date
+      }
+    }
+  }
+`;
