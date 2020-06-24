d039fc1fe1bf4333997355813de1da32387f7407
jeroentiggelman <infos@photographicdesign.be>
2020-06-24 16:26:40 +0200

    added more styles to global-style.js

--- src/theme/global-style.js
+++ src/theme/global-style.js
@@ -8,6 +8,17 @@ export const theme = {
     black: "#000",
     white: "#fff",
 
+    primary: {
+      100: "#aa7fd4",
+      200: "#9966cc",
+      300: "#884cc3",
+      400: "#773bb2",
+      500: "#663399",
+      600: "#552b80",
+      700: "#442266",
+      800: "#331a4d",
+      900: "#221133",
+    },
     grey: {
       100: "#f7fafc",
       200: "#edf2f7",
@@ -25,6 +36,90 @@ export const theme = {
     serif: "Pridi, serif",
     monospace: '"Space Mono", monospace',
   },
+  fontSize: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    base: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "4rem",
+  },
+  fontWeight: {
+    hairline: "100",
+    thin: "200",
+    light: "300",
+    normal: "400",
+    medium: "500",
+    semibold: "600",
+    bold: "700",
+    extrabold: "800",
+    black: "900",
+  },
+  boxShadow: {
+    default: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+    md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+    lg:
+      "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+    xl:
+      "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+    "2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+    inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
+    outline: "0 0 0 3px rgba(66, 153, 225, 0.5)",
+    none: "none",
+  },
+  letterSpacing: {
+    tighter: "-0.05em",
+    tight: "-0.025em",
+    normal: "0",
+    wide: "0.025em",
+    wider: "0.05em",
+    widest: "0.1em",
+  },
+  lineHeight: {
+    none: "1",
+    tight: "1.25",
+    snug: "1.375",
+    normal: "1.5",
+    relaxed: "1.625",
+    loose: "2",
+  },
+  listStyleType: {
+    none: "none",
+    disc: "disc",
+    decimal: "decimal",
+  },
+  spacing: {
+    px: "1px",
+    "0": "0",
+    "1": "0.25rem",
+    "2": "0.5rem",
+    "3": "0.75rem",
+    "4": "1rem",
+    "5": "1.25rem",
+    "6": "1.5rem",
+    "8": "2rem",
+    "10": "2.5rem",
+    "12": "3rem",
+    "16": "4rem",
+    "20": "5rem",
+    "24": "6rem",
+    "32": "8rem",
+    "40": "10rem",
+    "48": "12rem",
+    "56": "14rem",
+    "64": "16rem",
+  },
+  borderRadius: {
+    none: "0",
+    sm: "0.125rem",
+    default: "0.25rem",
+    lg: "0.5rem",
+    full: "9999px",
+  },
 };
 
 export const GlobalStyle = createGlobalStyle`
@@ -37,9 +132,16 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     scroll-behavior: smooth;
     font-family: ${({ theme }) => theme.fonts.sans};
+    font-size: ${({ theme }) => theme.fontSize.xl};
     color: ${({ theme }) => theme.colours.grey[900]};
   }
   body {
-    line-height: 1.9;
+    line-height: ${({ theme }) => theme.lineHeight.norma};
+    letter-spacing: ${({ theme }) => theme.letterSpacing.wide};
+    background-color: ${({ theme }) => theme.colours.grey[100]};
+    
+  }
+  .youtube-mdx-embed {
+    margin-top: ${({ theme }) => theme.spacing[8]};
   }
 `;
